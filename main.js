f4edcdce6f3b1d0dccc716cf0cb725b91d82f981
Subin Jang <wkdtnqls0506@naver.com>
2023-04-28 15:32:49 +0900

    JS : Navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 스크롤될 때 navbar 배경색 바꿈
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+})
