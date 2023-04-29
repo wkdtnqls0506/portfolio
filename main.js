36ab10ccc0b43a6364233683dd06bd3ec6f58660
Subin Jang <wkdtnqls0506@naver.com>
2023-04-29 16:51:41 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener("scroll", () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
-})
+});
 
 // navbar item을 클릭했을 때 해당 페이지로 스크롤링 -> data-link를 통해 5가지 경우를 하나의 코드에 담아야 하기 때문에 데이터셋 사용
 const navbarMenu = document.querySelector(".navbar__menu");
@@ -20,13 +20,38 @@ navbarMenu.addEventListener("click", (event) => {
         return;
     }
     scrollIntoViews(link);
-})
+});
 
 // "contact me" 버튼을 클릭하였을 때, contact 페이지로 스크롤링 -> contact button의 경우 한가지 경우만 존재하기 때문에 직접 #contact를 사용해도 무관
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
     scrollIntoViews('#contact');
-})
+});
+
+// scroll이 내려갈수록 home 페이지가 점점 투명해지도록 설정
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function scrollIntoViews(selector) {
     const scrollTo = document.querySelector(selector);
